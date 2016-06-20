6813971cf9f0af6ea1156aaa3a55952632ac6dbb
Hariton Batkov <yanker90@gmail.com>
2016-06-20 14:38:35 +0300

    hate java and Android studio

--- app/src/main/java/com/pproduct/datasource/core/data_structure/DataStructure.java
+++ app/src/main/java/com/pproduct/datasource/core/data_structure/DataStructure.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
 
 import com.pproduct.datasource.core.listeners.DataObject;
 
